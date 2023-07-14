cb76f82802d40a6efc4b9a2e6938a4180ebb43bb
siddhesh1983 <34923865+siddhesh1983@users.noreply.github.com>
2023-07-14 15:47:01 -0500

    Add files via upload

---
+++ BaseLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsoleApp
+{
+    public abstract class BaseLogic
+    {
+
+        internal string firstName = string.Empty;
+        internal string lastName = string.Empty;
+
+        public BaseLogic(string firstNameData, string lastNameData)
+        {
+            firstName = firstNameData;
+            lastName = lastNameData;
+
+        }
+        public abstract string GetFormattedString();
+    }
+       
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PracticeConsoleApp;
+
+Console.WriteLine("Hello, World!");
+
+BaseLogic baseLogic = new Logic("First name" , "Last Name");
+
+BaseLogic derivedLogic = new DerivedLogic("Second name", "Second last Name");
+
+baseLogic.GetFormattedString();
+derivedLogic.GetFormattedString();
+
+Console.ReadLine();
